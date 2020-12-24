69498bd7b3651c3ff640a3d10b817fcf06c1e0ab
Andrew Briscoe <admin@pandasportal.net>
2020-12-24 23:33:23 +0800

    Added devicenames uuid pairsings

--- physical_disks.py
+++ physical_disks.py
@@ -1,13 +1,18 @@
-from os import listdir
+from os import listdir, readlink
 #from os.path import isfile, join
-
+from os.path import islink, join, basename
 DISK_UUID_DIR = "/dev/disk/by-uuid/"
 
 def fetchDiskUuids():
-    return [f for f in listdir(DISK_UUID_DIR)]
+    return [uuid for uuid in listdir(DISK_UUID_DIR)]
+
+
+def fetchDiskUuidNamePairs():
+    return [(uuid,basename(readlink(join(DISK_UUID_DIR,uuid)))) for uuid in fetchDiskUuids()]
+
 
 def main():
-    print(fetchDiskUuids())
+    print(fetchDiskUuidNamePairs())
 
 if __name__ == "__main__":
     main()
